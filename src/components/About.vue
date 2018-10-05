680ed33fb4db175ee63b66aa6f4d08d4d8b2f928
Zack Ensign <zensign@MacBook-Pro.home>
2018-10-04 22:14:39 -0400

    Finishing initial styling and contents

--- src/components/About.vue
+++ src/components/About.vue
@@ -7,10 +7,10 @@
       </div>
       <div class="content-col">
         <p>
-          If you haven't caught this by now, my name is <strong>Zack Ensign</strong>. I've been been developing Apps and Websites for over a dozen years. I've worked with good, bad, and ugly technologies and frameworks. On desktop, mobile, and physical installations. For start-ups, boutique shops, and corporations. Across a few different states. It's been a hell of a ride!
+          Hello, friend. My name is <strong>Zack Ensign</strong>. I build websites and applications and have done so for the last 12+ years. I enjoy developing on the front-end crafting delightful user experiences, but also work behind the scenes to create architectures in the cloud and build pipelines to power the delivery of those experiences. My career has taken me though a variety of industries and I feel like the journey has made me a better developer. My greatest strength is an open mind to learn, grow, and always improve my craft.
         </p>
         <p>
-          When I'm not doing that, I'm trying to juggle writing and recording of original music. Sometimes by myself, but often with friends and bandmates. Guitars, drums, microphones, and synths make me happy.
+          When I'm not creating with code, I enjoy writing and recording music. I've self-taught myself various audio recording applications and the fundamentals of audio engineering. I've built my own personal studio where I record with friends and bandmates. I play guitar, bass and make weird noises with synths and keyboards. I am a connoisseur of musical equipment and probably spend a little too much time admiring unique and beautiful gear online.
         </p>
       </div>
     </div>
